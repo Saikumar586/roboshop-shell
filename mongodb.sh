69a0c45f1d36001b40e36fbc35c0a487ffb43a48
psaikumar <psaikumar586@gmail.com>
2023-08-31 14:31:35 +0530

    new changes reboshell

--- mongodb.sh
+++ mongodb.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 USERID=$(id -u)
-R="\e[32m" #'\e[0;32m'
-G="\e[33m"
+R="\e[31m" #'\e[0;32m'
+G="\e[32m"
 N="\e[0m"
 FILENAME=$0
 DATE=$(date +%F)
 LOGFILE=/tmp/$DATE-$FILENAME
 
-if [ USERID -ne 0];
+if [ USERID -ne 0 ];
 then 
     echo -e " Error : $R pls run with root user $N"
     exit
@@ -26,19 +26,15 @@ fi
 }
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
-
 VALIDATE $? "copy the mongo db repo into yum repos d" 
 
 yum install mongodb-org -y &>>$LOGFILE
-
 VALIDATE $? "Mongo db installed" 
 
 systemctl enable mongod &>>$LOGFILE
-
 VALIDATE $? "Enable mongo db" 
 
 systemctl start mongod &>>$LOGFILE
-
 VALIDATE $? "start mongo db" 
 
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>$LOGFILE
