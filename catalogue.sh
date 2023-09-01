8ff1d38b0f1ee6df4a4884870816f7f31683ec0f
psaikumar <psaikumar586@gmail.com>
2023-09-01 13:01:58 +0530

    roboshop shell

--- catalogue.sh
+++ catalogue.sh
@@ -7,6 +7,7 @@ DATE=$(date +%F)
 LOGFILE=/tmp/$DATE-$FILENAME
 url=$(curl -sL https://rpm.nodesource.com/setup_lts.x | bash)
 
+
 if [ $USERID -ne 0 ]
 then 
      echo -e "Error : $R pls run with root user $N"
@@ -24,33 +25,102 @@ else
 fi
 }
 
-$url &<< $LOGFILE
+$url &>> $LOGFILE
 VALIDATE $? " RPM NODESOURCE INSTALLED"
 
-yum install nodejs 
+yum install nodejs  &>> $LOGFILE
 VALIDATE $? " NodeJs INSTALLED"
 
-echo -n "Enter the username: "
-read text
-useradd $text
+# echo "Enter a user name: "
+# read name
 
-echo "Enter directory name"
-read dirname
+# if grep -w $name /etc/passwd > /dev/null
+# then
+#     echo "user $name is on this system."
+# else
+#     echo "user $name does not exist." 
+# fi
 
-if [ ! -d "$app" ]
-then
-    echo "File doesn't exist. Creating now"
-    mkdir ./$app
-    echo "File created"
-else
-    echo "File exists"
-fi
+# echo "Enter a group name: "
+# read name
 
-curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>>$LOGFILE
 
-VALIDATE $? "donwloadoing NPMFile"
+useradd $name &>> $LOGFILE
+#VALIDATE $? " user added"
 
-yum install nodejs 
 
-VALIDATE $? "install nodejs"
+mkdir /app &>> $LOGFILE
+#VALIDATE $? " make directory"
+
+
+# echo -n "Enter the username: "
+# # read text
+# # useradd $text
+# if [ $uid -eq 0 ] then 
+#     echo user exist
+# else 
+
+ 
+# echo "Enter directory name"
+# read dirname
+# unzip /tmp/catalogue.zip
+# if [ ! -d "$app" ]
+# then
+#     echo "File doesn't exist. Creating now"
+#     mkdir ./$app
+#     echo "File created"
+# else
+#     echo "File exists"
+# fi
+
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> $LOGFILE
+
+VALIDATE $? "open the zip file"
+
+cd /app /  &>> $LOGFILE
+VALIDATE $? " change the directory"
+
+
+unzip /tmp/catalogue.zip &>> $LOGFILE
+VALIDATE $? " unzip the file"
+
+
+cd /app &>> $LOGFILE
+VALIDATE $? " change dir "
+
+
+npm install  &>> $LOGFILE
+VALIDATE $? " install NPM "
+
+
+cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>> $LOGFILE
+VALIDATE $? " copy the catalogue service "
+
+
+systemctl daemon-reload &>> $LOGFILE
+VALIDATE $? " reload  "
+
+
+systemctl enable catalogue &>> $LOGFILE
+VALIDATE $? " enable "
+
+
+systemctl start catalogue &>> $LOGFILE
+VALIDATE $? "start catalogue  "
+
+
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+VALIDATE $? "copy mongo repo  "
+
+
+yum install mongodb-org-shell -y &>> $LOGFILE
+VALIDATE $? "install mongo  "
+
+
+mongo --host mongodb.saidev.world </app/schema/catalogue.js &>> $LOGFILE
+VALIDATE $? " provide host name : "
+
+
+
+
 
