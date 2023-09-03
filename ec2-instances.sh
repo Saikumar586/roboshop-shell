75bd214fdabccd63ab7865a966d7c7c19183ccc7
psaikumar <psaikumar586@gmail.com>
2023-09-03 23:06:00 +0530

    create instance

--- ec2-instances.sh
+++ ec2-instances.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
 
-NAMES=( "Mongodb" "catalogue" "redis" "mysql" "rabbitmq" "user" "payment" "dispatch" "web" )
-
+NAMES=( "Mongodb" "catalogue" "redis" "mysql" "rabbitmq" "user" "cart" "payment" "shipping" "dispatch" "web" )
+INSTANCE_TYPE=""
+# if its mongodb or mysql then t3.medium other t2.micro
 for i in "${NAMES[@]}"
 do 
-echo "create instances: $i"
+
+if [[ $i == "Mongodb" || $i == "mysql" ]]
+then 
+    INSTANCE_TYPE="t3.medium"
+else 
+    INSTANCE_TYPE="t2.micro"
+fi
+echo "create $i instance"
+   CREATE_INSTANCE=$(aws ec2 run-instances --image-id ami-03265a0778a880afb  --instance-type t2.micro  --security-group-ids 
+    sg-0bf973e946668a277 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" || jq -r '.Instances[0].PrivateIpAddress')
+echo "create $I instance ip $CREATE_INSTANCE"
 done
 
