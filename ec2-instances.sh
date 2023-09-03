430e3ef5dd9f5c1f2d733adf46d79aca60ae5d29
psaikumar <psaikumar586@gmail.com>
2023-09-03 23:46:34 +0530

    create instance

--- ec2-instances.sh
+++ ec2-instances.sh
@@ -3,6 +3,7 @@
 
 NAMES=( "Mongodb" "catalogue" "redis" "mysql" "rabbitmq" "user" "cart" "payment" "shipping" "dispatch" "web" )
 INSTANCE_TYPE=""
+DOMAIN_NAME=saidev.world
 # if its mongodb or mysql then t3.medium other t2.micro
 for i in "${NAMES[@]}"
 do 
@@ -16,5 +17,22 @@ fi
 echo "create $i instance"
    CREATE_INSTANCE=$(aws ec2 run-instances --image-id ami-03265a0778a880afb  --instance-type t2.micro  --security-group-ids sg-0bf973e946668a277 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
 echo "create $i instance ip $CREATE_INSTANCE"
+
+aws route53 change-resource-record-sets --hosted-zone-id Z0559164Y728U4VCBF55 --change-batch 
+{
+            "Comment": "CREATE A Record ",
+            "Changes": [
+        {
+            "Action": "CREATE",
+                        "ResourceRecordSet":
+                         {
+                                    "Name": "$i.$DOMAIN_NAME",
+                                    "Type": "A",
+                                    "TTL": 1,
+                                 "ResourceRecords":[{ "Value": "$CREATE_INSTANCE"}]
+                         }
+        }
+                        ]
+}
 done
 
