459c04b7dece1dcf0e3d2cd3080ab3fd4e55f0a3
psaikumar <psaikumar586@gmail.com>
2023-09-25 20:16:50 +0530

    changes

--- ec2-instances.sh
+++ ec2-instances.sh
@@ -15,7 +15,7 @@ else
     INSTANCE_TYPE="t2.micro"
 fi
 echo "create $i instance"
-   CREATE_INSTANCE=$(aws ec2 run-instances --image-id ami-03265a0778a880afb  --instance-type t2.micro  --security-group-ids sg-0bf973e946668a277 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+   CREATE_INSTANCE=$(aws ec2 run-instances --image-id ami-03265a0778a880afb  --instance-type t2.micro  --security-group-ids sg-028da1a64b2b90013 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
 echo "create $i instance ip $CREATE_INSTANCE"
 
 aws route53 change-resource-record-sets --hosted-zone-id Z0525532325ZF78I0E7EM --change-batch '
