9117f9c01a8385f653da56b6652ed03e22b62af8
psaikumar <psaikumar586@gmail.com>
2023-09-29 11:59:14 +0530

    shell-roboshop

--- ec2-instances.sh
+++ ec2-instances.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-NAMES= $@ #( "Mongodb" "catalogue" "redis" "mysql" "rabbitmq" "user" "cart" "payment" "shipping" "dispatch" "web" )
+NAMES= ( "Mongodb" "catalogue" "redis" "mysql" "rabbitmq" "user" "cart" "payment" "shipping" "dispatch" "web" )
 INSTANCE_TYPE=""
 DOMAIN_NAME=saidev.world
 # if its mongodb or mysql then t3.medium other t2.micro
-for i in "${@}"
+for i in "${NAMES}"
 do 
 
 if [[ $i == "Mongodb" || $i == "mysql" ]]
@@ -18,7 +18,7 @@ echo "create $i instance"
    CREATE_INSTANCE=$(aws ec2 run-instances --image-id ami-03265a0778a880afb  --instance-type t2.micro  --security-group-ids sg-028da1a64b2b90013 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
 echo "create $i instance ip $CREATE_INSTANCE"
 
-aws route53 change-resource-record-sets --hosted-zone-id Z04789002E1QBH1QMXME7 --change-batch '
+aws route53 change-resource-record-sets --hosted-zone-id Z05693643DQZHBZI213XV --change-batch '
 {
             "Comment": "CREATE A Record ",
             "Changes": [
@@ -28,7 +28,7 @@ aws route53 change-resource-record-sets --hosted-zone-id Z04789002E1QBH1QMXME7 -
                          {  
                                     "Name": "'$i.$DOMAIN_NAME'",
                                     "Type": "A",
-                                    "TTL": 1,
+                                    "TTL": 1,       
                                  "ResourceRecords":[{ "Value": "'$CREATE_INSTANCE'"}]
                          }
         }
